29493cc2f342123fc9d1bac640aeff3534f095ce
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-02-03 12:19:01 +0000

    adminとuserのコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :user do
+    get 'homes/top'
+    get 'homes/about'
+  end
+
+# ユーザー用
+# URL /users/sign_in ...
+  devise_for :users, controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+  }
+  
+# 管理者用
+# URL /admin/sign_in ...
+  devise_for :admins, controllers: {
+  sessions: "admin/sessions"
+  }
+  
+  
+  namespace :admin do
+    root to: "homes#top"
+  end
+  
+  scope module: :user do
+    root to: "homes#top"
+  end
 end
